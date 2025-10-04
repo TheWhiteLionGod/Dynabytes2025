282b09bc5bad8ba13e448dd728558e78c1b3ee19
Aryan Agarwal <aaryan12jul@gmail.com>
2025-10-04 11:37:17 -0400

    Merged Auto from Virtual Robot Simulator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Game.java
@@ -4,35 +4,28 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.auto.AutoPickAndShoot;
-import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 @TeleOp(name="Game", group="FTC2025")
 public class Game extends LinearOpMode {
-    SampleMecanumDrive drive;
     Robot auto;
     Robot teleop;
 
     @Override
     public void runOpMode() {
-        drive = new SampleMecanumDrive(hardwareMap);
         auto = new AutoPickAndShoot();
-        teleop = new LeftDrive();
+        teleop = new Dynabytes2025();
 
         waitForStart();
 
         // Starting Autonomous
         auto.configure();
-        auto.drive = drive;
 
         auto.game = this;
         auto.run();
 
-        // Fixing Drive Position
-        drive.setPoseEstimate(auto.drive.getPoseEstimate());
-
         // Starting TeleOp
         teleop.configure();
-        teleop.drive = drive;
+        teleop.drive = auto.drive; // Transferring Auto Position Data
 
         teleop.game = this;
         teleop.run();
