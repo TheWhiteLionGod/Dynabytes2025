b37df7205dda90c7da11ab59eb8de58d50aa3e61
Aryan Agarwal <aaryan12jul@gmail.com>
2026-02-14 11:25:36 -0500

    Created Servo Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ServoTest.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.Dynawheels;
+
+@TeleOp(name="Servo Test", group="TEST")
+public class ServoTest extends Dynawheels {
+    Servo servo;
+    @Override
+    public void config() {
+        servo = hardwareMap.get(Servo.class, "Gate");
+    }
+
+    @Override
+    public void run() {
+        while (opModeIsActive()) {
+            if (gamepad1.yWasPressed()) {
+                servo.setPosition(servo.getPosition() + 0.1);
+            }
+            else if (gamepad1.aWasPressed()) {
+                servo.setPosition(servo.getPosition() - 0.1);
+            }
+
+            telemetry.addData("Servo Pos", servo.getPosition());
+            telemetry.update();
+        }
+    }
+
+    @Override
+    public void cleanup() {
+
+    }
+}
